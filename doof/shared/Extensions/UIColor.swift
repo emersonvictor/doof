132271acb4cc0666955c90fb138837b00ec1d9d0
Emerson Victor <emersonvvictor@gmail.com>
2018-11-14 11:45:40 -0300

    Add UI elements and fix modal height

---
+++ doof/shared/Extensions/UIColor.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor.swift
+//  doof
+//
+//  Created by Emerson Victor on 14/11/18.
+//  Copyright © 2018 BDSM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    // Pink
+    static let darkPink = UIColor(red:0.75, green:0.19, blue:0.89, alpha:1.0)
+    // Gray - Text
+    static let gray = UIColor(red:0.34, green:0.32, blue:0.38, alpha:1.0)
+}
