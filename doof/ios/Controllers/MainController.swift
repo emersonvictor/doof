c42206b559883219821ea5b99b74788c4b502083
João Melo <jgvm@academy.cin.ufpe.br>
2018-11-21 23:34:05 -0300

    First SpriteKit Attempt

--- doof/ios/Controllers/MainController.swift
+++ doof/ios/Controllers/MainController.swift
@@ -18,10 +18,12 @@ class MainController: UIViewController {
     var energyTimer = Timer()
     var isSleeping = false
     var userSelectedSleepingTime = 8.0
-    //
+    // Haptic Feedback
     let selectionFeedback = UISelectionFeedbackGenerator()
     // Slide transition
     lazy var slideTransitioningDelegate = SlidePresentationManager()
+    // SpriteKit
+    var scene:GuiozaScene?
     
     
     // MARK: Outlets
@@ -55,9 +57,10 @@ class MainController: UIViewController {
         customProgessBars(progressBar: happinessProgressView)
         
         // SpriteKit
-        let scene = SKScene(fileNamed: "MainScene.sks")
-        scene?.scaleMode = .aspectFill
-        self.mainSKView.presentScene(scene)
+//        let scene = SKScene(fileNamed: "MainScene.sks")
+//        scene?.scaleMode = .aspectFill
+//        self.mainSKView.presentScene(scene)
+        
         
         // Hide interaction buttons
         let x: CGFloat = 60
@@ -65,7 +68,34 @@ class MainController: UIViewController {
         self.waterButton.transform = CGAffineTransform(translationX: x, y: y)
         self.foodButton.transform = CGAffineTransform(translationX: 0, y: 60)
         self.sleepButton.transform = CGAffineTransform(translationX: -x, y: y)
+        
+        
+        //########################################################################
+        // MARK: Teste!!!
+        //SpriteKit
+        
     }
+    
+    
+    @IBOutlet weak var testeOutlt: UIButton!
+    
+    @IBAction func showGuioza(_ sender: Any) {
+        if let scene = self.scene {
+            scene.jumpGuioza()
+            testeOutlt.alpha = 0
+        }
+    }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.scene = GuiozaScene(size: CGSize(width: self.mainSKView.frame.size.width, height: self.mainSKView.frame.size.height))
+        self.mainSKView.presentScene(scene)
+    }
+    //########################################################################
+    
     
     
     // MARK: Main Button Interaction
